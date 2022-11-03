fcac6813e7f2276e0e5c2f4cc163c23ec3606d82
Nagurshareef Shaik <nagurshareef123@gmail.com>
2022-11-03 22:22:52 +0530

    initial commit
    this is the first commit for the project of life saving project

---
+++ src/main/java/com/donateblood/com/donateblood/DonatebloodApplication.java
@@ -0,0 +1,13 @@
+package com.donateblood.com.donateblood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatebloodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatebloodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donateblood/com/donateblood/DonatebloodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donateblood.com.donateblood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatebloodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
